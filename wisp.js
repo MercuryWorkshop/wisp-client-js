51ad95a6d912ec404c20284f0cded40c0b5c4e62
ading2210 <ading2019@gmail.com>
2024-01-26 19:29:37 -0500

    fix continue packet support

--- wisp.js
+++ wisp.js
@@ -3,6 +3,7 @@ const protocol_string = "wisp-v1"
 function uint_from_array(array) {
   if (array.length == 4) return new Uint32Array(array.buffer)[0];
   else if (array.length == 2) return new Uint16Array(array.buffer)[0];
+  else if (array.length == 1) return array[0];
   else throw "invalid array length";
 }
 
